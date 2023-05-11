a2f745676185c13f26925cd34a32551219f88edb
Damir2302 <damir23.02@mail.ru>
2023-05-11 18:13:47 +0300

    fixed

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -3,12 +3,33 @@ $(document).ready(function () {
     // ДЛЯ ВСЕХ(!!!!!!) ВСПЛЫВАЮЩИХ ОКОН [Если кликаем за пределы всплывающего окна, закрываем это окно]
     $("html").on("click", function (e) {
         if (
-            !$(e.target).closest(".navbar").length
+            !$(e.target).closest(".cat__tabs").length
         ) {
             $("body").removeClass("overflow-hidden")
             $('#page').removeClass('bg-overlay')
+
+            $('.cat__tabs').removeClass('active')
         }
     });
+
+    // CATALOG INNER TABS
+    $('.cat__tabs-select').on('click', function() {
+        $(this).parent().toggleClass('active')
+    })
+
+    if ($(window).width() < 1024) {
+        $('.cat__tabs-btn[data-selected]').on('click', function() {
+            $('.cat__tabs-btn').removeClass('active')
+            $(this).addClass('active')
+
+            $('.cat__tabs-select').text($(this).text())
+
+            $('.cat__tabs').toggleClass('active')
+
+            $('.cat__content').removeClass('active')
+            $(`.cat__content[data-active=${$(this).attr('data-selected')}]`).addClass('active')
+        })
+    }
 
 
 });
