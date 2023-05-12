8980fdb7bdaf5734f18b05847eb4679f5ed1c154
Damir2302 <damir23.02@mail.ru>
2023-05-12 18:05:52 +0300

    fixed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+    let anchors = document.querySelectorAll('.js-anchor')
+    if (anchors.length > 0) {
+        anchors.forEach(anchor => {
+            anchor.onclick = function (e) {
+                e.preventDefault();
+                document.querySelector(`#${anchor.href.split('#')[1]}`).scrollIntoView({
+                    block: 'start',
+                    behavior: 'smooth',
+                })
+            }
+        })
+    }
+
     // POPULAR SLIDER
     $('.popular__tab-item').on('click', function() {
         $('.popular__tab-item').removeClass('active')
@@ -25,6 +38,15 @@ $(document).ready(function() {
     $('.analyzes__item-title').on('click', function() {
         $(this).parent().toggleClass('expanded')
         $(this).parent().find('.analyzes__item-wrap').slideToggle()
+    })
+
+    // CONTACTS TABS
+    $('.contacts__tab-btn[data-show]').on('click', function() {
+        $('.contacts__tab-btn').removeClass('active')
+        $(this).addClass('active')
+
+        $('.contacts__list').removeClass('active')
+        $(`.contacts__list[data-active=${$(this).attr('data-show')}]`).addClass('active')
     })
     
     // FOOTER TAB
--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -3,12 +3,14 @@ $(document).ready(function () {
     // ДЛЯ ВСЕХ(!!!!!!) ВСПЛЫВАЮЩИХ ОКОН [Если кликаем за пределы всплывающего окна, закрываем это окно]
     $("html").on("click", function (e) {
         if (
-            !$(e.target).closest(".cat__tabs").length
+            !$(e.target).closest(".cat__tabs").length &&
+            !$(e.target).closest(".contacts__select").length
         ) {
             $("body").removeClass("overflow-hidden")
             $('#page').removeClass('bg-overlay')
 
             $('.cat__tabs').removeClass('active')
+            $('.contacts__select').removeClass('active')
         }
     });
 
@@ -31,5 +33,28 @@ $(document).ready(function () {
         })
     }
 
+    if ($(window).width() >= 1024) {
+        $('.cat__tabs-btn[data-selected]').on('click', function() {
+            $('.cat__tabs-btn').removeClass('active')
+            $(this).addClass('active')
+
+            $('.cat__content').removeClass('active')
+            $(`.cat__content[data-active=${$(this).attr('data-selected')}]`).addClass('active')
+        })
+    }
+
+    // CONTACTS PAGE SELECT
+    $('.contacts__select-value').on('click', function() {
+        $(this).parent().toggleClass('active')
+    })
+
+    $('.contacts__select-option').on('click', function() {
+        $('.contacts__select-option').removeClass('active')
+        $(this).addClass('active')
+
+        $('.contacts__select-value').text($(this).text())
+
+        $('.contacts__select').toggleClass('active')
+    })
 
 });
